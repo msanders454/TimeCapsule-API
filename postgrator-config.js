f3ae535d23d7993a9495c4e21130e2d1992448c1
Michael Sanders <mikesanderspsu@gmail.com>
2020-05-16 15:34:56 -0400

    Ready to Deploy

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": (process.env.NODE_ENV === 'development')
+  "connectionString": (process.env.NODE_ENV === 'test')
      ? process.env.TEST_DATABASE_URL
      : process.env.DATABASE_URL,
   "ssl": !!process.env.SSL,
