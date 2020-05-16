7b67f4f4ee73202d4bd7f0feaadd17cf8cab6dcc
Michael Sanders <mikesanderspsu@gmail.com>
2020-05-16 15:02:50 -0400

    Ready to Deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/time-capsule',
-    TEST_DATABASE_URL: process.env.TEST_DB_URL|| 'postgresql://dunder_mifflin@localhost/capsule-test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/time-capsule',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL|| 'postgresql://dunder_mifflin@localhost/capsule-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   }
